c052a84c91f85b77dc837554ba09421f2ee6f156
Liliana <liliana.gh@mail.ru>
2022-04-01 01:03:04 +0300

    localStorage

--- src/mst/stores/RootStore.store.ts
+++ src/mst/stores/RootStore.store.ts
@@ -1,8 +1,24 @@
+import { values } from 'mobx'
 import {types} from 'mobx-state-tree'
 import { ContentModel } from '../models/Content.model'
 
 
 const RootStore = types.model('RootStore', {
        contents: types.array(ContentModel)})
+
+.actions(() => ({
+       setDataToLocalStorage(any = {}){
+              localStorage.setItem('initialState','string')
+       },
+       getDataFromLocalStorage(string = 'initialState'){
+             const storage =  localStorage.getItem('')
+             console.log('>>storage', storage)
+             return storage
+       },
+       removeLocalStorage(string = 'initialState'){
+              localStorage.removeItem('')
+              console.log('>>$() was removed')
+       }
+}))
 
 export default RootStore
